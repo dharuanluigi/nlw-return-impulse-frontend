3c9272b56547296e24c665b6739838c7e1589bef
Dharuan Emina <dharuan.emina.tc@take.net>
2022-05-04 08:49:38 -0300

    Added: finished implements of front end funcs cl#02

--- src/components/widgetForm/index.tsx
+++ src/components/widgetForm/index.tsx
@@ -6,6 +6,7 @@ import thoughtImageUrl from '../../assets/thought.svg';
 
 import { FeedbackTypeStep } from "./steps/FeedbackTypeStep";
 import { FeedbackContentStep } from "./steps/FeedbackContentStep";
+import { FeedbackSuccessStep } from "./steps/FeedbackSuccessStep";
 
 export const feedbackTypes = {
     BUG: {
@@ -35,21 +36,30 @@ export type FeedbackType = keyof typeof feedbackTypes;
 
 export function WidgetForm() {
     const [feedbackType, setFeedbackType] = useState<FeedbackType | null>(null)
+    const [feedbackSent, setFeedbackSent] = useState(false);
 
     function handleRestarFeedback() {
         setFeedbackType(null);
+        setFeedbackSent(false);
     }
 
     return (
         <div className="bg-zinc-900 p-4 relative rounded-2xl mb-4 flex flex-col items-center shadow-lg w-[calc(100vw-2rem)] md:w-auto">
 
-            {!feedbackType ? (
-                <FeedbackTypeStep onFeedbackTypeChanged={setFeedbackType} />
+            {feedbackSent ? (
+                <FeedbackSuccessStep onRestarFeedbackRequest={handleRestarFeedback} />
             ) : (
-                <FeedbackContentStep 
-                    feedbackType={feedbackType}
-                    onFeedbackRestartRequested={handleRestarFeedback} 
-                />
+                <>
+                    {!feedbackType ? (
+                        <FeedbackTypeStep onFeedbackTypeChanged={setFeedbackType} />
+                    ) : (
+                        <FeedbackContentStep 
+                            feedbackType={feedbackType}
+                            onFeedbackRestartRequested={handleRestarFeedback} 
+                            onFeedbackSent={() => setFeedbackSent(true)}
+                        />
+                    )}
+                </>
             )}
 
             <footer className="text-xs text-neutral-400">
