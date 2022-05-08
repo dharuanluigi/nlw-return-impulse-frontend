3d2431116b06a95622162f737af27190675a13e3
Dharuan Emina <dharuan.emina.tc@take.net>
2022-05-08 07:44:44 -0300

    Fix: type for svg imports

--- src/components/widgetForm/index.tsx
+++ src/components/widgetForm/index.tsx
@@ -1,70 +1,76 @@
 import { useState } from "react";
 
-import bugImageUrl from '../../assets/bug.svg';
-import ideaImageUrl from '../../assets/idea.svg';
-import thoughtImageUrl from '../../assets/thought.svg';
+import bugImageUrl from "../../assets/bug.svg";
+import ideaImageUrl from "../../assets/idea.svg";
+import thoughtImageUrl from "../../assets/thought.svg";
 
 import { FeedbackTypeStep } from "./steps/FeedbackTypeStep";
 import { FeedbackContentStep } from "./steps/FeedbackContentStep";
 import { FeedbackSuccessStep } from "./steps/FeedbackSuccessStep";
 
 export const feedbackTypes = {
-    BUG: {
-        title: 'Problema',
-        image: {
-            source: bugImageUrl,
-            alt: 'Imagem de um inseto'
-        }
+  BUG: {
+    title: "Problema",
+    image: {
+      source: bugImageUrl,
+      alt: "Imagem de um inseto",
     },
-    IDEA: {
-        title: 'Ideia',
-        image: {
-            source: ideaImageUrl,
-            alt: 'Imagem de uma lâmpada'
-        }
+  },
+  IDEA: {
+    title: "Ideia",
+    image: {
+      source: ideaImageUrl,
+      alt: "Imagem de uma lâmpada",
     },
-    OTHER: {
-        title: 'Outro',
-        image: {
-            source: thoughtImageUrl,
-            alt: 'Imagem de uma nuvem de pensamento'
-        }
-    }
-}
+  },
+  OTHER: {
+    title: "Outro",
+    image: {
+      source: thoughtImageUrl,
+      alt: "Imagem de uma nuvem de pensamento",
+    },
+  },
+};
 
 export type FeedbackType = keyof typeof feedbackTypes;
 
 export function WidgetForm() {
-    const [feedbackType, setFeedbackType] = useState<FeedbackType | null>(null)
-    const [feedbackSent, setFeedbackSent] = useState(false);
+  const [feedbackType, setFeedbackType] = useState<FeedbackType | null>(null);
+  const [feedbackSent, setFeedbackSent] = useState(false);
 
-    function handleRestarFeedback() {
-        setFeedbackType(null);
-        setFeedbackSent(false);
-    }
+  function handleRestarFeedback() {
+    setFeedbackType(null);
+    setFeedbackSent(false);
+  }
 
-    return (
-        <div className="bg-zinc-900 p-4 relative rounded-2xl mb-4 flex flex-col items-center shadow-lg w-[calc(100vw-2rem)] md:w-auto">
+  return (
+    <div className="bg-zinc-900 p-4 relative rounded-2xl mb-4 flex flex-col items-center shadow-lg w-[calc(100vw-2rem)] md:w-auto">
+      {feedbackSent ? (
+        <FeedbackSuccessStep onRestarFeedbackRequest={handleRestarFeedback} />
+      ) : (
+        <>
+          {!feedbackType ? (
+            <FeedbackTypeStep onFeedbackTypeChanged={setFeedbackType} />
+          ) : (
+            <FeedbackContentStep
+              feedbackType={feedbackType}
+              onFeedbackRestartRequested={handleRestarFeedback}
+              onFeedbackSent={() => setFeedbackSent(true)}
+            />
+          )}
+        </>
+      )}
 
-            {feedbackSent ? (
-                <FeedbackSuccessStep onRestarFeedbackRequest={handleRestarFeedback} />
-            ) : (
-                <>
-                    {!feedbackType ? (
-                        <FeedbackTypeStep onFeedbackTypeChanged={setFeedbackType} />
-                    ) : (
-                        <FeedbackContentStep 
-                            feedbackType={feedbackType}
-                            onFeedbackRestartRequested={handleRestarFeedback} 
-                            onFeedbackSent={() => setFeedbackSent(true)}
-                        />
-                    )}
-                </>
-            )}
-
-            <footer className="text-xs text-neutral-400">
-                Feito com ♥ pela <a className="underline underline-offset-2" target="_blank" href="https://www.rocketseat.com.br">Rocketseat</a>
-            </footer>
-        </div>
-    )
+      <footer className="text-xs text-neutral-400">
+        Feito com ♥ pela{" "}
+        <a
+          className="underline underline-offset-2"
+          target="_blank"
+          href="https://www.rocketseat.com.br"
+        >
+          Rocketseat
+        </a>
+      </footer>
+    </div>
+  );
 }
